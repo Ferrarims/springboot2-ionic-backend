b61bbee1653cb4909f6f15f96948877b3b95bacf
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-21 13:58:52 +0100

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/ferrari/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.ferrari.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ferrari.cursomc.domains.Cliente;
+import com.ferrari.cursomc.repositories.ClienteRepository;
+import com.ferrari.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName() + "."));
+	}
+}
