985aaff5a5e229ca4c1f3cd32afed3d4d0946402
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-21 11:58:11 +0100

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ferrari/cursomc/services/CategoriaService.java
+++ src/main/java/com/ferrari/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.ferrari.cursomc.domains.Categoria;
 import com.ferrari.cursomc.repositories.CategoriaRepository;
+import com.ferrari.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName() + "."));
 	}
 }
