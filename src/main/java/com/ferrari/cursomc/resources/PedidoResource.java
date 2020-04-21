2489c8050d39329a9ae1a442389bd41afb5ca916
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-21 17:44:20 +0100

    Inserindo novo Cliente com POST

--- src/main/java/com/ferrari/cursomc/resources/PedidoResource.java
+++ src/main/java/com/ferrari/cursomc/resources/PedidoResource.java
@@ -18,7 +18,7 @@ public class PedidoResource {
 	private PedidoService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> listar(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Pedido obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
