94af702555ea275452e5b15ff44f02c11eac44bd
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-28 16:07:13 +0100

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/com/ferrari/cursomc/services/ClienteService.java
+++ src/main/java/com/ferrari/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ferrari.cursomc.domains.Cidade;
 import com.ferrari.cursomc.domains.Cliente;
 import com.ferrari.cursomc.domains.Endereco;
+import com.ferrari.cursomc.domains.enums.Perfil;
 import com.ferrari.cursomc.domains.enums.TipoCliente;
 import com.ferrari.cursomc.dto.ClienteDTO;
 import com.ferrari.cursomc.dto.ClienteNewDTO;
 import com.ferrari.cursomc.repositories.ClienteRepository;
 import com.ferrari.cursomc.repositories.EnderecoRepository;
+import com.ferrari.cursomc.security.UserSS;
+import com.ferrari.cursomc.services.exceptions.AuthorizationException;
 import com.ferrari.cursomc.services.exceptions.DataIntegrityException;
 import com.ferrari.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,11 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado.");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName() + "."));
