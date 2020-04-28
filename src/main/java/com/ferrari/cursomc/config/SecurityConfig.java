f59c935f4ecee73ff0e1e30e0b5ef6f784a36ef8
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-28 13:47:05 +0100

    Implementando autorização

--- src/main/java/com/ferrari/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ferrari/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ferrari.cursomc.security.JWTAuthenticationFilter;
+import com.ferrari.cursomc.security.JWTAuthorizationFilter;
 import com.ferrari.cursomc.security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
