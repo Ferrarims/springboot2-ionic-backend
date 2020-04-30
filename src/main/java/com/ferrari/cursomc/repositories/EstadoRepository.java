5348088e6585fcd8752d10d20b07070f6e10b347
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-30 18:55:39 +0100

    Endpoints para buscar estados e cidades

--- src/main/java/com/ferrari/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/ferrari/cursomc/repositories/EstadoRepository.java
@@ -1,11 +1,16 @@
 package com.ferrari.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ferrari.cursomc.domains.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
 
+	@Transactional(readOnly=true)
+	public List<Estado> findAllByOrderByNome();
 }
